0c051693265a7390e68cd48752b44c4588f2f67c
田島　和樹 <tajimakazuki@tajimakazukinoMacBook-puro.local>
2020-02-28 17:16:33 +0900

    add new file

---
+++ test.py
@@ -0,0 +1,28 @@
+"""
+このファイルに解答コードを書いてください
+"""
+list = []
+dic = {}
+count = 0
+number = 0
+with open("input.txt") as f:
+    for s_line in f:
+        list.append(s_line)
+        count = count + 1
+count = count - 1
+
+for i in range(count):
+    if int(list[-1]) % int(list[i][0]) == 0:
+        number = number + 1
+        a = list[i].find(":")
+        key = list[i][:a]
+        key = int(key)
+        value = list[i][a+1:]
+        dic[key] = value
+sortedDict = sorted(dic.items())
+
+if number == 0:
+    print(list[-1])
+else:
+    for i in range(number-1):
+        print(sortedDict[i][1])
